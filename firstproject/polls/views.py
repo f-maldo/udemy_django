05d5280923e74c1e6b73121de3d2338bed0e1f6e
Felipe Maldonado <ftmaldonadog@gmail.com>
2020-07-02 10:32:30 -0400

    Working process up to lesson 36

--- firstproject/polls/views.py
+++ firstproject/polls/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hola Mundo Django")
+    # return HttpResponse("Hola Mundo Django")
+    questions = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('index.html')
+    context = {
+        'questions': questions,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse("Detail: %s" % question_id)
+
+
+def results(request, question_id):
+    response = "Results: %s"
+    return response % question_id
+
+
+def vote(request, question_id):
+    return HttpResponse("Vote %s" % question_id)

05893a79785a2deb50807cd3739e198b021e66f2
Felipe Maldonado <ftmaldonadog@gmail.com>
2020-07-02 15:03:48 -0400

    Working process up to lesson 57

--- firstproject/polls/views.py
+++ firstproject/polls/views.py
@@ -1,40 +1,30 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(request):
-    # return HttpResponse("Hola Mundo Django")
-    questions = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('index.html')
-    context = {
-        'questions': questions,
-    }
-    return render(request, 'index.html', context)
-    # return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'questions'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    '''
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('Question does not exist')
-    '''
-    question = get_object_or_404(Question, pk=question_id)
-    # return HttpResponse("Detail %s" % question_id)
-    return render(request, 'detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'results.html'
 
 
 def vote(request, question_id):
-    # return HttpResponse("Vote %s" % question_id)
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])

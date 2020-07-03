c1fe490336bbcbd5e9ddd1e13f72b97d255d9457
Felipe Maldonado <ftmaldonadog@gmail.com>
2020-07-03 11:56:43 -0400

    Working process up to lesson 88

--- secondproject/secondproject/settings.py
+++ secondproject/secondproject/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8080",
+]
+
 
 # Application definition
 
@@ -39,11 +45,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'listelement.apps.ListelementConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
---
+++ vueproject/src/components/List.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <div v-for="e in elements" v-bind:key="e.id">
+            <b-card :title="e.title">
+                <b-card-text>{{e.description}}</b-card-text>
+            </b-card>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "List",
+        created() {
+            this.findAll()
+        },
+        data(){
+            return {
+                elements: []
+            };
+        },
+        methods: {
+            findAll: function () {
+                fetch("http://127.0.0.1:8000/api/element/?format=json")
+                    .then(response => response.json())
+                    .then(response => this.elements = response)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .box{
+        border: 3px solid lightgray;
+        margin: 5px 0 0 0;
+    }
+</style>
